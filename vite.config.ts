c9472633de16e649ccd21ac3c25cf7fd618404a2
sinhzuitinh-cloud <sinhzuitinh@gmail.com>
2026-09-10 01:07:19 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import {defineConfig, Plugin} from 'vite';
 // LINT.IfChange(aistudio_media_plugin)
 function aistudioMediaPlugin(): Plugin {
   return {
-    base: '/NS-English-Management/'
+    base: '/NS-English-Management/',
     name: 'vite-plugin-aistudio-media',
     configureServer(server) {
       server.middlewares.use((req, res, next) => {
